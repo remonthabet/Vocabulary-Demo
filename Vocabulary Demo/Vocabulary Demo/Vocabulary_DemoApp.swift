f93cefc6c0f83c751b43dc3d0a2d0e1c5662488e
remon thabet <>
2025-02-05 23:30:33 +0200

    first commit

---
+++ Vocabulary Demo/Vocabulary Demo/Vocabulary_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vocabulary_DemoApp.swift
+//  Vocabulary Demo
+//
+//  Created by remon on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Vocabulary_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
